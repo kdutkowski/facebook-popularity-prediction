ab6a1105b791eb3dda12682b267ed6562543c8c2
Jakub Dutkowski <kdutkowski@gmail.com>
2016-09-27 19:32:44 +0200

    calculate and plot regression results

--- popularityPrediction.R
+++ popularityPrediction.R
@@ -1,6 +1,10 @@
 if (!require("ggplot2")) install.packages("ggplot2", repos="http://cran.rstudio.com/")
+if (!require("reshape2")) install.packages("reshape2", repos="http://cran.rstudio.com/")
+if (!require("caret")) install.packages("caret", repos="http://cran.rstudio.com/")
 
 library(ggplot2)
+library(reshape2)
+library(caret)
 
 # Read all data from file into a data frame
 rawData <- read.csv('data/data.csv', header = FALSE)
@@ -13,26 +17,54 @@ removeOutliers <- function(dataFrame, colIndex) {
   return(dataFrame[dataFrame[, colIndex] > sigma3.min & dataFrame[, colIndex] < sigma3.max,])
 }
 
+rMse <- function(trueValue, predictedValue) {
+  return(mean((predictedValue/trueValue - 1)^2))
+}
+
 # Analyse the basic statistics of the v(n) or n = 24, 72, 168.
 summary(rawData[, c(24, 72, 168) + 1])
-
-# Extract the values of v(168)
-dataWithoutOutliers <- removeOutliers(rawData, 169)
 
 # Plot distribution histogram 
 ggplot(rawData, aes(x=rawData[, 169])) + 
   geom_histogram(bins = 50)
 
 # Plot logx distribution histogram
-ggplot(rawData, aes(x=rawData[, 169])) + 
-  geom_histogram(bins = 50) +
-  scale_x_log10()
+rawData$log169 <- log(rawData[, 169])
+ggplot(rawData, aes(x=rawData$log169)) + 
+  geom_histogram(bins = 50)
+
+# Extract the values of v(168)
+dataWithoutOutliers <- removeOutliers(rawData, "log169")
 
 # Plot logx distribution histogram without outliers
-ggplot(dataWithoutOutliers, aes(x=dataWithoutOutliers[, 169])) + 
-  geom_histogram(bins = 50) +
-  scale_x_log10()
+ggplot(dataWithoutOutliers, aes(x=dataWithoutOutliers$log169)) + 
+  geom_histogram(bins = 50)
 
 # Correlation between inputs 1:24 and output
-correlationIndexes = 1:24 + 1
-correlations <- lapply(dataWithoutOutliers[, correlationIndexes], function (x) { cor(x, dataWithoutOutliers[, 169]) })
+regressionParametersIndices = 1:24 + 1
+correlations <- sapply(dataWithoutOutliers[, regressionParametersIndices], function (x) { cor(x, dataWithoutOutliers$log169) })
+
+# Split data to train and test
+inTraining <- createDataPartition(dataWithoutOutliers$log169, p = .9, 
+                                  list = FALSE)
+trainData <- dataWithoutOutliers[ inTraining,]
+testData <- dataWithoutOutliers[-inTraining,]
+
+results <- data.frame(time = 1:length(regressionParametersIndices))
+
+results$singleInput <- sapply(regressionParametersIndices, function(x) {
+  model <- lm(as.formula(paste("log169 ~", colnames(trainData)[x])), data = trainData)
+  predicted <- predict(model, testData)
+  return(rMse(testData$log169, predicted))
+})
+
+results$multipleInputs <- sapply(regressionParametersIndices, function(x) {
+  model <- lm(as.formula(paste("log169 ~", paste(colnames(trainData)[2:x], collapse = "+"))), data = trainData)
+  predicted <- predict(model, testData)
+  return(rMse(testData$log169, predicted))
+})
+
+meltedResults <- melt(results, id="time")
+ggplot(data=meltedResults, aes(x=time, y=value, colour=variable, shape=variable)) +
+        geom_point() +
+        geom_line()
