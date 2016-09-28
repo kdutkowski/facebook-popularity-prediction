48fdc362820fd18b1da4c92e83ad8b3df68c0cec
Jakub Dutkowski <kdutkowski@gmail.com>
2016-09-28 14:49:52 +0200

    read row names (to fix indices of hours

--- popularityPrediction.R
+++ popularityPrediction.R
@@ -10,7 +10,7 @@ library(reshape2)
 library(caret)
 
 # Read all data from file into a data frame
-rawData <- read.csv('data/data.csv', header = FALSE)
+rawData <- read.csv('data/data.csv', row.names = 1, header = FALSE, col.names = c("id", paste("h", 1:168, sep = "")))
 
 # A function that removes outliers from data frame according to
 # 3-sigma rule, based on column specified with colIndex
@@ -30,55 +30,55 @@ rMse <- function(trueValue, predictedValue) {
 
 # Analyse the basic statistics of the v(n) or n = 24, 72, 168.
 statisticsParametersIndices <- c(24, 72, 168)
-summary(rawData[, statisticsParametersIndices + 1])
+summary(rawData[, statisticsParametersIndices])
 
 # Plot distribution histogram
-ggplot(rawData, aes(x = rawData[, 169])) +
+ggplot(rawData, aes(x = rawData[, 168])) +
   geom_histogram(bins = 50)
 
 # Plot logx distribution histogram
-rawData$log169 <- log(rawData[, 169])
-ggplot(rawData, aes(x = rawData$log169)) +
+rawData$log168 <- log(rawData[, 168])
+ggplot(rawData, aes(x = rawData$log168)) +
   geom_histogram(bins = 50)
 
 # Extract the values of v(168)
-dataWithoutOutliers <- removeOutliers(rawData, "log169")
+dataWithoutOutliers <- removeOutliers(rawData, "log168")
 
 # Plot logx distribution histogram without outliers
-ggplot(dataWithoutOutliers, aes(x = dataWithoutOutliers$log169)) +
+ggplot(dataWithoutOutliers, aes(x = dataWithoutOutliers$log168)) +
   geom_histogram(bins = 50)
 
 # Correlation between inputs 1:24 and output
-regressionParametersIndices = 1:24 + 1
+regressionParametersIndices = 1:24
 correlations <- sapply(dataWithoutOutliers[, regressionParametersIndices], function (x) {
-  cor(x, dataWithoutOutliers$log169)
+  cor(x, dataWithoutOutliers$log168)
 })
 
 # Split data to train and test
-inTraining <- createDataPartition(dataWithoutOutliers$log169, p = .9,
+inTraining <- createDataPartition(dataWithoutOutliers$log168, p = .9,
                                   list = FALSE)
 trainData <- dataWithoutOutliers[inTraining, ]
 testData <- dataWithoutOutliers[-inTraining, ]
 
 # Initialize the data frame for results
-results <- data.frame(time = 1:length(regressionParametersIndices))
+results <- data.frame(time = regressionParametersIndices)
 
 # For each reference time create the single parameter model and calculate rMSE
 results$singleInput <- sapply(regressionParametersIndices, function(x) {
   model <-
-    lm(as.formula(paste("log169 ~", colnames(trainData)[x])), data = trainData)
+    lm(as.formula(paste("log168 ~", colnames(trainData)[x])), data = trainData)
   predicted <- predict(model, testData)
-  return(rMse(testData$log169, predicted))
+  return(rMse(testData$log168, predicted))
 })
 
 # The same for model built on all views preceding the reference time
 results$multipleInputs <- sapply(regressionParametersIndices, function(x) {
   model <-
     lm(as.formula(paste(
-      "log169 ~", paste(colnames(trainData)[2:x], collapse = "+")
+      "log168 ~", paste(colnames(trainData)[1:x], collapse = "+")
     )), data = trainData)
   predicted <- predict(model, testData)
-  return(rMse(testData$log169, predicted))
+  return(rMse(testData$log168, predicted))
 })
 
 # Plot mRSE for both models
